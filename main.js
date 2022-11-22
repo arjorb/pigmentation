ed553a14a3816ab5765a99350156b3cef909e2d4
John U <arjorb@gmail.com>
2022-11-22 18:42:43 +0200

    fixing some issues

--- main.js
+++ main.js
@@ -4,10 +4,37 @@ const button = document.querySelector('#button');
 const body = document.querySelector('body');
 
 function handleColor(color) {
-  body.style.backgroundColor = color;
-  span.style.color = color;
+  const random = Math.floor(Math.random() * color.length);
+  const generatedColor = color[random];
+  body.style.backgroundColor = generatedColor;
+  span.style.color = generatedColor;
+
+  switch (generatedColor) {
+    case 'yellow':
+      span.innerHTML = 'Yellow';
+      break;
+
+    case 'red':
+      span.innerHTML = 'Red';
+      break;
+
+    case '#D6E4E5':
+      span.innerHTML = 'Zircon';
+      break;
+
+    case '#8BBCCC':
+      span.innerHTML = 'Seagull';
+      break;
+
+    case '#BA135D':
+      span.innerHTML = 'Jazzberry';
+      break;
+
+    default:
+      span.innerHTML = 'White';
+  }
 }
 
-const yellow = 'yellow';
+const arr = ['yellow', 'red', '#D6E4E5', '#8BBCCC', '#BA135D'];
 
-button.addEventListener('click', () => handleColor(yellow));
+button.addEventListener('click', () => handleColor(arr));
